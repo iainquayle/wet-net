a2603d3bb746ed7509f5c9777d2be30ae9827639
iainquayle <36175014+iainquayle@users.noreply.github.com>
2023-04-06 03:35:15 -0600

    not much

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from weather_dataset import WeatherDataset, plot
+import torchvision
+import random
 
 AIRMASS = "airmass"
 SANDWICH = "sandwich"
@@ -8,8 +10,7 @@ MICROCLOUD = "microcloud"
 if __name__ == '__main__':
 	#dataset = WeatherDataset("data", [AIRMASS, SANDWICH, MICROCLOUD], 1)	
 	dataset = WeatherDataset("data", [AIRMASS], 1)	
-	plot(dataset, 0, 2)
-
+	plot(dataset, 0, 1)
 
 	for i in range(20):
 		print(dataset.matching_file_names[i])
